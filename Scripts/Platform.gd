788162c3e1a33a4cdcf81de171d1babbe9e67872
Touff-97 <se.vas@live.com>
2022-03-04 02:05:45 +0100

    added moving platforms, with variable moving offset. Bug: horizontal platforms don't move player.

---
+++ Scripts/Platform.gd
@@ -0,0 +1,50 @@
+extends StaticBody
+
+onready var platform_position : Vector3 = translation
+onready var tween : Tween = $Tween
+
+export(bool) onready var is_vertical
+export(float) onready var translation_offset
+
+
+func _ready() -> void:
+	if is_vertical:
+		move_in_y_axis()
+	else:
+		move_in_x_axis()
+
+
+func move_in_y_axis() -> void:
+	var animation_time : float = 6.0
+	# warning-ignore:return_value_discarded
+	tween.interpolate_property(self, "translation", \
+							   Vector3(platform_position.x, platform_position.y, platform_position.z), \
+							   Vector3(platform_position.x, platform_position.y - translation_offset, platform_position.z), \
+							   animation_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+	platform_position = Vector3(platform_position.x, platform_position.y - translation_offset, platform_position.z)
+	# warning-ignore:return_value_discarded
+	tween.interpolate_property(self, "translation", \
+							   Vector3(platform_position.x, platform_position.y, platform_position.z), \
+							   Vector3(platform_position.x, platform_position.y + translation_offset, platform_position.z), \
+							   animation_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, animation_time)
+	tween.repeat = true
+	# warning-ignore:return_value_discarded
+	tween.start()
+
+
+func move_in_x_axis() -> void:
+	var animation_time : float = 6.0
+	# warning-ignore:return_value_discarded
+	tween.interpolate_property(self, "translation", \
+							   Vector3(platform_position.x, platform_position.y, platform_position.z), \
+							   Vector3(platform_position.x - translation_offset, platform_position.y, platform_position.z), \
+							   animation_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0)
+	platform_position = Vector3(platform_position.x - translation_offset, platform_position.y, platform_position.z)
+	# warning-ignore:return_value_discarded
+	tween.interpolate_property(self, "translation", \
+							   Vector3(platform_position.x, platform_position.y, platform_position.z), \
+							   Vector3(platform_position.x + translation_offset, platform_position.y, platform_position.z), \
+							   animation_time, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, animation_time)
+	tween.repeat = true
+	# warning-ignore:return_value_discarded
+	tween.start()
